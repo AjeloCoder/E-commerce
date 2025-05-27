3a402519d816ef38a4d5fca66086f15d2e1a9a4c
Alejo Bermudez <aebmbu@gmail.com>
2025-05-27 18:05:59 -0300

    Prueba GH

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/', // Base para desarrollo
+  }
+
+  // Para builds de producción (como para GitHub Pages)
+  if (command === 'build') {
+    // Reemplaza 'E-commerce' con el nombre EXACTO de tu repositorio de GitHub
+    config.base = '/E-commerce/'
+  }
+
+  return config
 })
